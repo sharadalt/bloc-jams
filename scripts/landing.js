e8c56e9be8852078fedba407cabe6b1522cd0c42
sharada123 <ltsharada@gmail.com>
2016-08-05 13:30:42 -0700

    chkpt_22_dom_scripting_animation_assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,29 +2,16 @@
  
                  var points = document.getElementsByClassName('point');
  
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 var revealPoints = function(pt) {
+                     points[pt].style.opacity = 1;
+                     points[pt].style.transform = "scaleX(1) translateY(0)";
+                     points[pt].style.msTransform = "scaleX(1) translateY(0)";
+                     points[pt].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
+        
+                 for(var i=0; i <points.length; i++){
+	               revealPoints(i);
+                 }
+        
+                
+      };
