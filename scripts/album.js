a4bbd14e842998251392f0bf27486f6eb0aaf941
sharada123 <ltsharada@gmail.com>
2016-08-17 19:40:23 -0700

    chkpt_25_dom_scripting_album_view_assignment

--- scripts/album.js
+++ scripts/album.js
@@ -38,11 +38,11 @@
      year: '2016',
      albumArtUrl: 'assets/images/album_covers/21.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+         { title: 'Life is beautiful', duration: '1:01' },
+         { title: 'Take care of nature', duration: '5:01' },
+         { title: 'Birds are flying', duration: '3:21'},
+         { title: 'Water flowing', duration: '3:14' },
+         { title: 'Trees waving', duration: '2:15'}
      ]
  };
 
@@ -57,30 +57,37 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
-
-var setCurrentAlbum = function(album) {
-     // #1
+     // elements to populate dynamically
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
+var setCurrentAlbum = function(album) {
+    
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
      albumSongList.innerHTML = '';
  
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
  
  window.onload = function() {
-     setCurrentAlbum(albumSharada);
+   setCurrentAlbum(albumSharada);
+   
+   var albums = [albumPicasso, albumMarconi, albumSharada];
+   index = 0;
+     albumImage.addEventListener("click", function(event){
+       setCurrentAlbum(albums[index++]);
+       if (index == albums.length){
+         index = 0;
+       }
+                                 
+    });
  };
