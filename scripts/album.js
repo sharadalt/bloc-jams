d5e6a8708f83555e3248613374a7ad8b177b84e8
sharada123 <ltsharada@gmail.com>
2016-09-11 21:30:34 -0700

    checkpoint_33_jquery_Buzz_Library_assignment

--- scripts/album.js
+++ scripts/album.js
@@ -210,6 +210,42 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
  var currentVolume = 80;
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $barPlayer  = $('.main-controls .play-pause');
+
+var togglePlayFromPlayerBar = function() {
+       //var songNumber = $(this).attr('data-song-number');
+       var songNumber = parseInt($(this).attr('data-song-number'));
+
+	   if (currentlyPlayingSongNumber !== null) {
+		  // Revert to song number for currently playing song because user started playing new song.
+		  var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+		  currentlyPlayingCell.html(currentlyPlayingSongNumber);
+	   }
+	   if (currentlyPlayingSongNumber !== songNumber) {
+		  // Switch from Play -> Pause button to indicate new song is playing.
+          setSong(songNumber);
+          currentSoundFile.play();
+		  $(this).html(pauseButtonTemplate);
+          currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+          updatePlayerBarSong();
+	   } else if (currentlyPlayingSongNumber === songNumber) {
+		  if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();   
+            }
+       }
+       
+     };
+
+$(document).ready(function() {
+   $barPlayer.click(togglePlayFromPlayerBar);
+  
+ });
  
  $(document).ready(function() {
    setCurrentAlbum(albumPicasso);
